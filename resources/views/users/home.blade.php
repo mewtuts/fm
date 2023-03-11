482a29e8b58efaf65938cb635b2df6f658030b9f
nathanielteria <102508838+nathanielteria@users.noreply.github.com>
2023-03-11 21:07:35 +0800

    create new file templatefile.blade.php

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet"/>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     @vite('resources/css/app.css')
     <title>Templates</title>
     <style>
+        /* Inline CSS */
     </style>
 </head>
 <body class="font-poppins bg-slate-50">  
